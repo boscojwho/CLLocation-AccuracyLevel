6744c31e1936f169e7ac555b676d5cb062def05a
J.w. Bosco Ho <boscojwho@gmail.com>
2013-08-03 21:22:37 -0700

    Modified description.

--- CLLocation+AccuracyLevel/CLLocation+AccuracyLevel/CLLocation+AccuracyLevel.h
+++ CLLocation+AccuracyLevel/CLLocation+AccuracyLevel/CLLocation+AccuracyLevel.h
@@ -6,7 +6,7 @@
 //
 
 /*
- ABSTRACT: CLLocation+AccuracyLevel categorizes the geographic accuracy level of a given placemark or region into one of the CLLocationAccuracyLevel enum values.
+ ABSTRACT: CLLocation+AccuracyLevel assigns a geographic accuracy level to any given placemark into one of the CLLocationAccuracyLevel enum values.
 
  The CLLocationAccuracyLevel enum mimics the available properties of a CLPlacemark object. In other words, a CLLocationAccuracyLevel is assigned based on the availability of those properties for a given MK/CLPlacemark object (e.g. if subThroughfare == nil, then CLLocationAccuracyLevel must be CLLocationAccuracyLevelThroughfare or higher).
  
